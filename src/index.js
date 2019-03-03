730d0e425f8ade9375f33a53d2fe20afb27dba51
Max Pikulin <trurl1000@gmail.com>
2019-03-03 16:32:04 +0000

    rootReducer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { createStore } from 'redux';
-// import { Provider } from "react-redux";
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+import rootReducer from './reducers/rootReducer';
 
-// const store = createStore();
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {};
+
+function rootReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
