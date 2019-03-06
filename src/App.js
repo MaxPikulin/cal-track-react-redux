5bac219e9df949b04a593bb46ceef31ec41443b9
Max Pikulin <trurl1000@gmail.com>
2019-03-06 18:12:29 +0000

    start generalisation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import Inputs from './components/Inputs';
 import Button from './components/Button';
+import CalsIn100 from './components/CalsIn100';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Inputs />
+        <CalsIn100 />
         <Button />
       </div>
     );
   }
 }
+// Create generic component for inputs(label from props).
 
 export default App;
